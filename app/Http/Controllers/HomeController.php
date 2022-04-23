2b80f22689a8c2a1c45d64e48ead3a06481f9c59
SANTOS-R12 <85270837+SANTOS-R12@users.noreply.github.com>
2022-04-23 17:33:55 -0600

    Project
    full project

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+
+    public function indexprod()
+    {
+        return view('productos.index');
+    }
+
+    public function index()
+    {
+        return view('home');
+    }
+}
---
+++ app/Models/Productos.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+//use Illuminate\Database\Eloquent\Factories\HasFactory;
+//use Illuminate\Database\Eloquent\Model;
+use Jenssegers\Mongodb\Eloquent\Model;
+
+class Productos extends Model
+{
+    protected $fillable = ["codprod", "nombreprod","descriprod", "typprod", "costunit", "cantprod"];
+    protected $connection = 'mongodb';
+    protected $collection = 'productos';
+}
